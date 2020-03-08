c9031017edc29a45b59dad6012079d494a043fd9
Ctrl Cmd <unalarisa@yahoo.com>
2020-03-08 20:46:24 +0100

    minor fixes

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 import requests
-import json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import APIException
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -13,7 +12,7 @@ def shopify_api(request):
 
     url = 'https://upsquaresandbox.myshopify.com/api/graphql'
     json = {'query': query}
-    access_token = "your token here"
+    access_token = "your token here!"
     headers = {
         "Content-Type": "application/json",
         "X-Shopify-Storefront-Access-Token": access_token
@@ -21,25 +20,25 @@ def shopify_api(request):
 
     apirequest = requests.post(url=url, json=json, headers=headers)
     if apirequest.status_code == 200:
-        results = apirequest.json()
-        nodes = results['data']['products']['edges']
+        apiresponse = apirequest.json()
+        nodes = apiresponse['data']['products']['edges']
         paginator = Paginator(nodes, 10)
         page = request.GET.get('page')
 
         try:
             result = paginator.page(page)
-            titles = result.object_list
+            objects = result.object_list
 
         except PageNotAnInteger:
             result = paginator.page(1)
-            titles = result.object_list
+            objects = result.object_list
         except EmptyPage:
             result = paginator.page(paginator.num_pages)
-            titles = result.object_list
+            objects = result.object_list
 
     else:
 
-        raise Exception("Query failed to run by returning code of {}. {}".format(
+        raise APIException("GraphQL query failed to run by returning code of {}. {}".format(
             apirequest.status_code, query))
 
-    return Response(titles)
+    return Response(objects)
